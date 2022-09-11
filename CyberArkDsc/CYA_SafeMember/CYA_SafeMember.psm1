ec3f99e4af3110ba160d181e285870762c469248
Tim Schindler <tim@iosharp.com>
2022-09-11 11:59:34 +0200

    refactor test-safemember

--- CyberArkDsc/CYA_SafeMember/CYA_SafeMember.psm1
+++ CyberArkDsc/CYA_SafeMember/CYA_SafeMember.psm1
@@ -182,86 +182,57 @@ function Test-SafeMember {
         [Ensure]$Ensure,
 
         [string]$SafeName,
-
         [string]$MemberName,
-
         [datetime]$MembershipExpirationDate,
-
         [boolean]$UseAccounts,
-
         [boolean]$RetrieveAccounts,
-
         [boolean]$ListAccounts,
-
         [boolean]$AddAccounts,
-
         [boolean]$UpdateAccountContent,
-
         [boolean]$UpdateAccountProperties,
-
         [boolean]$InitiateCPMAccountManagementOperations,
-
         [boolean]$SpecifyNextAccountContent,
-
         [boolean]$RenameAccounts,
-
         [boolean]$DeleteAccounts,
-
         [boolean]$UnlockAccounts,
-
         [boolean]$ManageSafe,
-
         [boolean]$ManageSafeMembers,
-
         [boolean]$BackupSafe,
-
         [boolean]$ViewAuditLog,
-
         [boolean]$ViewSafeMembers,
-
         [boolean]$requestsAuthorizationLevel1,
-
         [boolean]$requestsAuthorizationLevel2,
-
         [boolean]$AccessWithoutConfirmation,
-
         [boolean]$CreateFolders,
-
         [boolean]$DeleteFolders,
-
         [boolean]$MoveAccountsAndFolders,
 
-        [parameter(Mandatory = $true)]
         [String] $PvwaUrl,
-
-        [parameter(Mandatory = $true)]
         [String] $AuthenticationType,
-
-        [parameter(Mandatory = $true)]
         [pscredential] $Credential,
-
         [bool] $SkipCertificateCheck
     )
 
+    $isDesiredState = $false
+
     Get-CyberArkSession -PvwaUrl $PvwaUrl -Credential $Credential -AuthenticationType $AuthenticationType -SkipCertificateCheck $SkipCertificateCheck
 
-    $DesiredState = $false
+    $CurrentState = Get-SafeMember -SafeName $SafeName -MemberName $MemberName -PvwaUrl $PvwaUrl -AuthenticationType $AuthenticationType -Credential $Credential -SkipCertificateCheck $SkipCertificateCheck
 
-    try {
-        $ResourceExists = Get-PASSafeMember -SafeName $SafeName -MemberName $MemberName -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Permissions | Where-Object { $_.UseAccounts -eq $UseAccounts -and $_.RetrieveAccounts -eq $RetrieveAccounts -and $_.ListAccounts -eq $ListAccounts -and $_.AddAccounts -eq $AddAccounts -and $_.UpdateAccountContent -eq $UpdateAccountContent -and $_.UpdateAccountProperties -eq $UpdateAccountProperties -and $_.InitiateCPMAccountManagementOperations -eq $InitiateCPMAccountManagementOperations -and $_.SpecifyNextAccountContent -eq $SpecifyNextAccountContent -and $_.RenameAccounts -eq $RenameAccounts -and $_.DeleteAccounts -eq $DeleteAccounts -and $_.UnlockAccounts -eq $UnlockAccounts -and $_.ManageSafe -eq $ManageSafe -and $_.ManageSafeMembers -eq $ManageSafeMembers -and $_.BackupSafe -eq $BackupSafe -and $_.ViewAuditLog -eq $ViewAuditLog -and $_.ViewSafeMembers -eq $ViewSafeMembers -and $_.requestsAuthorizationLevel1 -eq $requestsAuthorizationLevel1 -and $_.requestsAuthorizationLevel2 -eq $requestsAuthorizationLevel2 -and $_.AccessWithoutConfirmation -eq $AccessWithoutConfirmation -and $_.CreateFolders -eq $CreateFolders -and $_.DeleteFolders -eq $DeleteFolders -and $_.MoveAccountsAndFolders -eq $MoveAccountsAndFolders }
-    } catch {
-        $ResourceExists = $null
+    switch ($Ensure) {
+        'Absent' {
+            if ($CurrentState.Ensure -eq [Ensure]::Absent) {
+                $isDesiredState = $true
+            }
+        }
+        'Present' {
+            if ($CurrentState.Ensure -ne [Ensure]::Absent) {
+                $isDesiredState = $true
+            }
+        }
     }
 
-    if ($Ensure -eq [Ensure]::Present -and $null -ne $ResourceExists) {
-        $DesiredState = $true
-    }
-
-    if ($Ensure -eq [Ensure]::Absent -and $null -eq $ResourceExists) {
-        $DesiredState = $true
-    }
-
-    $DesiredState
+    return $isDesiredState
 }
 
 [DscResource()]
